e251472fa69606da99e7434231a257cd32ef52ae
DanSBol <145160031+DanSBol@users.noreply.github.com>
2024-05-13 21:55:29 +0300

    Update BookingRepository.java

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -12,24 +12,27 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     @Query(value = "select bo from Booking as bo order by startDate")
     List<Booking> getBooking();
 
-    @Query(value = "select bo from Booking as bo where startDate <= 'now()' and endDate >= 'now()' order by startDate")
+    @Query(value = "select bo from Booking as bo where startDate <= CURRENT_TIMESTAMP() and " +
+            "endDate >= CURRENT_TIMESTAMP() order by startDate")
     List<Booking> getBookingCurrent();
 
-    @Query(value = "select bo from Booking as bo where startDate > 'now()' order by startDate")
+    @Query(value = "select bo from Booking as bo where startDate > CURRENT_TIMESTAMP() order by startDate")
     List<Booking> getBookingFuture();
 
-    @Query(value = "select bo from Booking as bo where startDate > 'now()' and bo.item = ?1 order by startDate")
+    @Query(value = "select bo from Booking as bo where startDate > CURRENT_TIMESTAMP() and " +
+            "bo.item = ?1 order by startDate")
     List<Booking> getBookingOneFutureAllStatuses(Item item);
 
-    @Query(value = "select bo from Booking as bo where startDate > 'now()' and bo.item = ?1 and status='APPROVED'" +
-            " order by startDate")
+    @Query(value = "select bo from Booking as bo where startDate > CURRENT_TIMESTAMP() and " +
+            "bo.item = ?1 and status='APPROVED' order by startDate")
     List<Booking> getBookingOneFutureApproved(Item item);
 
-    @Query(value = "select bo from Booking as bo where endDate < 'now()' and status='APPROVED' order by startDate")
+    @Query(value = "select bo from Booking as bo where endDate < CURRENT_TIMESTAMP() and " +
+            "status='APPROVED' order by startDate")
     List<Booking> getBookingPast();
 
-    @Query(value = "select bo from Booking as bo where startDate <= 'now()' and status='APPROVED' and bo.item = ?1 " +
-            "order by endDate desc")
+    @Query(value = "select bo from Booking as bo where startDate <= CURRENT_TIMESTAMP() and " +
+            "status='APPROVED' and bo.item = ?1 order by endDate desc")
     List<Booking> getBookingOnePast(Item item);
 
     @Query(value = "select count(*) from Booking as bo where item = ?1 and " +
@@ -37,7 +40,7 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     int getCount(long itemId, LocalDateTime start, LocalDateTime end);
 
     @Query(value = "select bo from Booking as bo where bo.item = ?1 and bo.booker = ?2 and " +
-            "bo.status = 'APPROVED' and bo.endDate < 'now()' order by startDate")
+            "bo.status = 'APPROVED' and bo.endDate < CURRENT_TIMESTAMP() order by startDate")
     List<Booking> getBookingByItemAndBooker(Item item, User booker);
 
     List<Booking> getBookingByStatus(BookingStatus status);
