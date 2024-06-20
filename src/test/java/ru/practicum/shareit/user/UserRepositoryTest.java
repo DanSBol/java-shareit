df029188691c2e84dd6f8d9464d0710c542dfe1e
DanSBolt <dan.s.boltachev@yandex.ru>
2024-06-20 21:15:20 +0300

    feat: добавил функциональность request (спринт 15)

--- src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
@@ -7,15 +7,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @DataJpaTest(
         includeFilters = @ComponentScan.Filter(
